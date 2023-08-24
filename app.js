bc47b7a38e88f345eb9a486d0f85eac556bce9ff
Iryna B <114106094+Iryna-Bigdash@users.noreply.github.com>
2023-08-24 13:31:58 +0100

    added basic stracture
    added rotes in app.js

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
-const contactsRouter = require("./routes/api/contacts");
 const authorizeRouter = require("./routes/api/authorization");
+const tasksRouter = require("./routes/api/tasks");
+const reviewsRouter = require("./routes/api/reviews");
 
 const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use(express.static("public"));
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
 app.use("/users", authorizeRouter);
-app.use("/api/contacts", contactsRouter);
+// app.use("/api-docs");
+app.use("/api/tasks", tasksRouter);
+app.use("/api/reviews", reviewsRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
---
+++ services/index.js
@@ -0,0 +1,5 @@
+const reviewsServices = require("./reviewsServices");
+const tasksServices = require("./tasksServices");
+const usersServices = require("./usersServices");
+
+module.exports = { reviewsServices, tasksServices, usersServices };
