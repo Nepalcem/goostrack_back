09d9a08c68eda503fc9d6797cb6eb7a5d150955a
Iryna B <114106094+Iryna-Bigdash@users.noreply.github.com>
2023-08-25 14:57:38 +0100

    adde basic structure for reviews
    rename file HttpError in correct form;
    added schemas and model;
    added route get
    write controller for getting all reviews

--- helpers/index.js
+++ helpers/index.js
@@ -1,4 +1,4 @@
-const HttpError = require("./HtppError");
+const HttpError = require("./HttpError");
 const ctrlWrapper = require("./ctrlWrapper");
 const handleMongooseError = require("./handleMongooseError");
 const sendEmail = require("./sendEmail");
