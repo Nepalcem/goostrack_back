4a25f35d67eed30adff11991c8c800675aea1b57
Iryna B <114106094+Iryna-Bigdash@users.noreply.github.com>
2023-08-25 18:56:28 +0100

    added basic structure for Reviews

--- controllers/reviewsController/getAllReviews.js
+++ controllers/reviewsController/getAllReviews.js
@@ -1,46 +1,10 @@
 const Review = require('../../models/reviewModel');
 const ctrlWrapper = require('../../helpers/ctrlWrapper');
-const HttpError = require('../../helpers/HttpError');
-
-const pagination = require('../../utils/pagination');
 
 const getAll = async (req, res) => {
-  const { page: currentPage, limit: currentLimit } = req.query;
 
-  const { page, limit, skip } = pagination(currentPage, currentLimit);
-
-  const reviews = await Review.aggregate([
-    { $match: { owner: { $exists: true, $en: null } } },
-    { $lookup: { from: 'user', localField: 'owner', foreignField: '_id', as: 'owner' } },
-    { $unwind: '$owner' },
-    {
-      $project: {
-        _id: 1,
-        rating: 1,
-        comment: 1,
-        createdAt: 1,
-        updatedAt: 1,
-        owner: { _id: 1, name: 1, avatarUrl: 1 },
-      },
-    },
-    { $sort: { createdAt: -1 } },
-    { $skip: skip },
-    { $limit: limit },
-  ]);
-
-  if (!reviews) {
-    throw HttpError(404, 'Not found reviews');
-  }
-
-  const totalReviews = await Review.countDocuments();
-
-  res.json({
-    reviews,
-    totalReviews,
-    totalPages: Math.ceil(totalReviews / limit),
-    currentPage: page,
-    limit,
-  });
+    const result = await Review.find();
+    res.json(result)
 };
 
 module.exports = ctrlWrapper(getAll);
--- routes/api/reviews.js
+++ routes/api/reviews.js
@@ -3,7 +3,7 @@ const ctrl = require('../../controllers/reviewsController');
 
 const router = Router();
 
-// returns all feedback: GET http://localhost:5000/api/reviews
+// returns all feedback: GET http://localhost:8000/api/reviews
 router.get('/', ctrl.getAllReviews);
 
 module.exports = router;
