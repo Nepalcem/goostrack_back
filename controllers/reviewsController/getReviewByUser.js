3f887a201b023f3f99729255e07e342112ca05b3
Iryna B <114106094+Iryna-Bigdash@users.noreply.github.com>
2023-08-27 15:17:15 +0100

    added get review by Owner

--- controllers/reviewsController/getReviewByUser.js
+++ controllers/reviewsController/getReviewByUser.js
@@ -1,26 +1,23 @@
-const Review = require('../../models/review');
-const ctrlWrapper = require('../../decorators/ctrlWrapper');
-const HttpError = require('../../helpers/HttpError');
+const Review = require("../../models/reviewModel");
+const ctrlWrapper = require("../../helpers/ctrlWrapper");
+const HttpError = require("../../helpers/HttpError");
 
-// Отримати відгуки, які належать певному власнику
 const getReviewByUser = async (req, res) => {
-  // Отримуємо _id власника з об'єкта req.user (якщо він існує)
   const owner = req.user?._id;
 
-  // Перевірка, чи є власник у запиті
   if (!owner) {
-    throw HttpError(400, 'Missing owner');
+    throw HttpError(400, "Missing owner");
   }
 
-  // Знаходимо всі відгуки, які належать даному власнику
-  const reviews = await Review.find({ owner }).populate('owner', '_id name avatarUrl');
+  const reviews = await Review.find({ owner }).populate(
+    "owner",
+    "_id username avatarURL"
+  );
 
-  // Перевірка, чи були знайдені відгуки
   if (!reviews || reviews.length === 0) {
-    throw HttpError(404, 'No reviews found for this owner');
+    throw HttpError(404, "No reviews found for this owner");
   }
 
-  // Відправляємо відповідь зі списком відгуків та даними про власника
   res.status(200).json({ code: 200, reviews });
 };
 
--- services/reviewsServices.js
+++ services/reviewsServices.js
@@ -1,30 +1,6 @@
 const Review = require("../models/reviewModel");
 const { HttpError } = require("../helpers/HttpError");
 
-async function showReviewsByOwner(owner, pagination) {
-  const reviews = await Review
-    .find({ owner }, "-createdAt -updatedAt -__v", pagination)
-    .populate("owner", "name avatarUrl");
-
-  if (!reviews) {
-    throw HttpError(400, "Unable to fetch Review");
-  }
-
-  return reviews;
-}
-
-async function showReviewById(id) {
-  const review = await Review
-    .findById(id, "-createdAt -updatedAt -__v")
-    .populate("owner", "name avatarUrl");
-
-  if (!review) {
-    throw HttpError(400, "Unable to fetch Review");
-  }
-
-  return review;
-}
-
 async function update(id, data) {
   const review = await Review
     .findByIdAndUpdate(id, data, {
@@ -53,8 +29,6 @@ async function removeReview(id) {
 }
 
 module.exports = {
-  showReviewsByOwner,
-  showReviewById,
   update,
   removeReview,
 };
